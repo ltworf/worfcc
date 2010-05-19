f66f9c07ab4ae8067068a4f41c5e139a759b77a9
LtWorf <LtWorf@69e835b3-ca64-47af-9ab5-919dc348255e>
2010-05-19 14:43:15 +0000

    - Strange stuff about the mem
    git-svn-id: http://galileo.dmi.unict.it/svn/worfcc@99 69e835b3-ca64-47af-9ab5-919dc348255e

--- trunk/llvm/compiler.py
+++ trunk/llvm/compiler.py
@@ -65,7 +65,9 @@ def llvm_compile(filename):
     dname='%s/%s.ll' % (os.path.dirname(filename),mname)
     f=file(dname,"w")
     
-    
+    #Stuff
+    f.write('target datalayout = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:32:64-f32:32:32-f64:32:64-v64:64:64-v128:128:128-a0:0:64-f80:32:32"')
+
     #Declarations
     f.write('declare void @printInt(i32 %x)\n')
     f.write('declare void @printDouble(double %x)\n')
